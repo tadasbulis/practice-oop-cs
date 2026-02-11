f41c177ec138137756bce6f05ae6b7c430d9cb4f
Benas Ragaišis <benas.rag149@go.kauko.lt>
2026-02-11 10:38:05 +0200

    Init: Lab1

---
+++ Lab1.App/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rnd = new Random();
+bool allowChallenge = args.Contains("--challenge");
+int max = allowChallenge ? 5 : 4;
+int task = rnd.Next(1, max + 1);
+Console.WriteLine("{0}", task);
+if (task == 5)
+{
+    int fallback = rnd.Next(1, 5); // 1..4
+                                   // rodyti ir #5, ir fallback
+}
