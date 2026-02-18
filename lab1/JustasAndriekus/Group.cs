b0bd61c5cf12ecb35341e88e95c7159e87a57d69
Justas <justukasandriekus@gmail.com>
2026-02-18 10:14:48 +0200

    Add profile edit form

---
+++ lab1/JustasAndriekus/Group.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_c_2_lab1
+{
+    public class Group
+    {
+        public string Pavadinimas { get; set; }
+        public List<Student> Studentas { get; set; }
+
+        public Group(string pavadinimas)
+        {
+            Pavadinimas = pavadinimas;
+            Studentas = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            Studentas.Add(student);
+            Console.WriteLine($"Studentas {student.Vardas} pridėtas į grupę {Pavadinimas}");
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine($"\nGrupė: {Pavadinimas}");
+            Console.WriteLine("Studentų sąrašas:");
+            Console.WriteLine("****************************");
+            foreach (var student in Studentas)
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.WriteLine($"Išviso studentų: {Studentas.Count}\n");
+        }
+    }
+}
---
+++ lab1/JustasAndriekus/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP_c_2_lab1
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Vardas { get; set; }
+        public string ElPastas { get; set; }
+        public string Vidurkis { get; set; }
+
+        public Student(int id, string vardas, string elPastas, string vidurkis)
+        {
+            Id = id;
+            Vardas = vardas;
+            ElPastas = elPastas;
+            Vidurkis = vidurkis;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Vardas: {Vardas}, El. Paštas: {ElPastas}, Vidurkis: {Vidurkis}";
+        }
+    }
+}
